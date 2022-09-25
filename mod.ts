cd82c72788e5a24d4367f63b593c92646ba54d2e
Bastien DUMONT <bastienxs@gmail.com>
2022-09-25 11:54:52 +0200

    whitelist public

--- mod.ts
+++ mod.ts
@@ -1,4 +1,8 @@
-import { Application } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import {
+  Application,
+  Context,
+  send,
+} from "https://deno.land/x/oak@v11.1.0/mod.ts";
 
 const app = new Application();
 const PORT = 8000;
@@ -6,7 +10,7 @@ const PORT = 8000;
 app.use(async (ctx, next) => {
   await next();
   const time = ctx.response.headers.get("X-Response-Time");
-  console.log(`${ctx.request.method} ${ctx.request.url} ${time}`);
+  console.log(`${ctx.request.method} ${ctx.request.url}: ${time}`);
 });
 
 app.use(async (ctx, next) => {
@@ -14,6 +18,19 @@ app.use(async (ctx, next) => {
   await next();
   const delta = Date.now() - start;
   ctx.response.headers.set("X-Response-Time", `${delta}ms`);
+});
+
+app.use(async (ctx) => {
+  const filePath = ctx.request.url.pathname;
+  const fileWhiteList = [
+    "/index.html",
+    "/javacripts/script.js",
+    "stylesheets/style.css",
+    "/images/favicon.png",
+  ];
+  if (fileWhiteList.includes(filePath)) {
+    await send(ctx, filePath, { root: `${Deno.cwd()}/public` });
+  }
 });
 
 app.use(async (ctx, next) => {
