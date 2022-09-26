5c4fa0b85dbe45f7da9e7a67b3e2df9cd7c35b0e
Bastien DUMONT <bastienxs@gmail.com>
2022-09-26 18:35:53 +0200

    display planets list

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 let launches = [];
 
 const numberHeading = "No.".padStart(5);
@@ -31,11 +32,14 @@ function loadLaunches() {
 }
 
 function loadPlanets() {
-  // TODO: Once API is ready.
-  // const planetSelector = document.getElementById("planets-selector");
-  // planets.forEach((planet) => {
-  //   planetSelector.innerHTML += `<option value="${planet.kepler_name}">${planet.kepler_name}</option>`;
-  // });
+  return fetch("/planets")
+    .then((plantesResponse) => plantesResponse.json())
+    .then((planets) => {
+      const planetSelector = document.getElementById("planets-selector");
+      planets.forEach((planet) => {
+        planetSelector.innerHTML += `<option value="${planet.kepler_name}">${planet.kepler_name}</option>`;
+      });
+    });
 }
 
 function abortLaunch() {
